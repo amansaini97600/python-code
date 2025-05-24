59061a2382ab4e044e44c7843b9087ec2b23fe7d
amansaini97600 <amanks97600@gmail.com>
2025-05-24 16:04:14 +0530

    add data

---
+++ main.py
@@ -0,0 +1,47 @@
+# name = input("Enter your name : ")
+# name = name.upper()
+# name = name.lower()
+# name = name.title()
+# name = name.capitalize()  
+# name = name.count("n")
+# name = name.islower()
+# name = name.isupper()
+# name = name.istitle()
+# print(name)
+# a = int(input("Enter a number : "))
+# b = int(input("Enter a number : "))
+# if a > b:
+#     print(" a is greater than b")
+# else:
+#     print("b is greater than a")    
+
+# if a%2==0:
+#     print("this number is even")
+# else:
+#     print("this number is odd")
+
+# year = int(input("Enter a year : "))
+
+# if year%4==0 and year%100!=0 or year%400==0:
+#     # if year%4==0 and year%100!=0:
+#     print("Leap year")
+# else:
+#     print("not leap year")
+
+p = int(input("Enter your % : "))
+
+# if p>=33:
+#     print("Pass")
+# else:
+#     print("fail")
+
+
+# if p>0:
+#     print("positive")
+# else:
+#     print("negative")
+
+# if p>99 and p<1000:
+#     print("yes this number is three digit number")
+# else:
+#     print("no")
